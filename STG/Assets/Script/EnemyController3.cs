6b4637359636d8fd1e57ebd8beb3665f8e0ada26
YamamotoAsuka <shira71neko@gmail.com>
2023-04-14 23:26:19 +0900

    エネミー３の挙動修正

--- STG/Assets/Script/EnemyController3.cs
+++ STG/Assets/Script/EnemyController3.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemyController3 : MonoBehaviour
 {
     bool xyflg=false;//上下を切り替えるスイッチ（最初はオフ）。
-    bool Destroyflg=false;//ジグザグ運動とはける挙動を切り替えるスイッチ（最初はオフ）。
     void Start()
     {
         
@@ -13,11 +12,11 @@ public class EnemyController3 : MonoBehaviour
 
     void Update()
     {
-        if(xyflg==false&&Destroyflg==false)//スイッチが両方ともオフの時。
+        if(xyflg==false)//スイッチがオフの時。
         {
             MoveFalse();//左下に直進。
         }
-        if(xyflg==true&&Destroyflg==false)//上下がオンでジグザグがオフの時。
+        if(xyflg==true)//スイッチががオンの時。
         {
             MoveTrue();//左上に直進。
         }
@@ -28,14 +27,14 @@ public class EnemyController3 : MonoBehaviour
         Transform myTransform = this.transform;//オブジェクトの位置情報を取得。
 
         Vector3 pos = myTransform.position;//オブジェクトの座標を取得。
-        if(pos.x>=-4.194f&&pos.y>=-4.5f)//一番下に到達するまで。
+        if(pos.x>=-8.888f&&pos.y>=-4.5f)//一番下に到達するまで。
         {
             pos.x -= 0.01f;//左へ移動。
             pos.y -= 0.025f;//下へ移動。
         }
         if(pos.y<-4.5f)//一番下に到達したら。
         {
-            xyflg=true;//上下のスイッチオン。
+            xyflg=true;//スイッチオン。
         }    
  
         myTransform.position = pos;//オブジェクトの座標を更新。
@@ -45,14 +44,14 @@ public class EnemyController3 : MonoBehaviour
         Transform myTransform = this.transform;//オブジェクトの位置情報を取得。
 
         Vector3 pos = myTransform.position;//オブジェクトの座標を取得。
-        if(pos.x>=-4.194f&&pos.y<=4.5f)//一番上に到達するまで。
+        if(pos.x>=-8.888f&&pos.y<=4.5f)//一番上に到達するまで。
         {
             pos.x -= 0.01f;//左へ移動。
             pos.y += 0.025f;//上へ移動。
         }
         if(pos.y>4.5f)//一番下に到達したら。
         {
-            xyflg=false;//上下のスイッチをオフ。
+            xyflg=false;//スイッチをオフ。
         }
         myTransform.position = pos;//オブジェクトの座標を更新。
     }
@@ -61,15 +60,10 @@ public class EnemyController3 : MonoBehaviour
         Transform myTransform = this.transform;//オブジェクトの位置情報を取得。
 
         Vector3 pos = myTransform.position;//オブジェクトの座標を取得。
-        if(pos.x<=-4.194f)//ある程度近づいたら。
-        {
-            Destroyflg=true;//ジグザクのスイッチオン。
-            pos.y-=0.025f;//下にはける。
-        }
-        myTransform.position = pos;//オブジェクトの座標を更新。
-        if(pos.y<-5.5f)//完全に見切れたら。
+        if(pos.x<-8.888f)//見切れたら。
         {
             Destroy(this.gameObject);//エネミー３を破壊。
         }
+        myTransform.position = pos;//オブジェクトの座標を更新。
     }
 }

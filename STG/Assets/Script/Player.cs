df70c3bdf0e30ebf9342a6babb68ead4c28e71e2
itou aiki <itouaiki46@gmail.com>
2023-04-14 23:04:20 +0900

    プレイヤーオブジェクトとプログラムの追加
    動きのみ

---
+++ STG/Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //動かすプレイヤーのオブジェクト
+    public GameObject player;
+    //マウスのクリックした座標の取得
+    Vector3 touchWorldPosition;
+    
+    //移動のスピード？
+    public int speed=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))//左クリックされたら起動
+        {
+            //マウスでクリックした座標の取得しtouchScreenPositionに格納
+            Vector3 touchScreenPosition=Input.mousePosition;
+            //プレイヤーの奥行きを5.0に固定
+            touchScreenPosition.z=5.0f;
+            Camera camera=Camera.main;//ここ理解できてない
+            //touchWorldPositionに何かを格納？（ここも理解できてない）
+            touchWorldPosition=camera.ScreenToWorldPoint(touchScreenPosition);
+        }
+
+        //プレイヤーが指定座標に移動（詳しくはわからない）
+        player.transform.position=Vector3.MoveTowards(player.transform.position,touchWorldPosition
+    ,speed*Time.deltaTime*2);
+
+       
+    }
+}

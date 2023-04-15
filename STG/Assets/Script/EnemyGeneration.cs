1499841708b0df10c7341c733eaf8dc72b1be9ee
YamamotoAsuka <shira71neko@gmail.com>
2023-04-15 11:10:46 +0900

    敵弾の追加と生成、エネミー３の挙動の修正および弾の移動プログラムの追加

--- STG/Assets/Script/EnemyGeneration.cs
+++ STG/Assets/Script/EnemyGeneration.cs
@@ -9,6 +9,7 @@ public class EnemyGeneration : MonoBehaviour
     float Seconds1=0;//エネミー１の生成スピードを管理するための変数。
     float Seconds2=0;//エネミー２の生成スピードを管理するための変数。
     float Seconds3=0;//エネミー３の生成スピードを管理するための変数。
+    float AllSeconds=0;
     public GameObject Enemy1;//エネミー１を指定するための格納ポイント。
     public GameObject Enemy2;//エネミー２を指定するための格納ポイント。
     public GameObject Enemy3;//エネミー３を指定するための格納ポイント。
@@ -24,23 +25,25 @@ public class EnemyGeneration : MonoBehaviour
         Seconds1+=Time.deltaTime;//時間を計測している。
         Seconds2+=Time.deltaTime;//時間を計測している。
         Seconds3+=Time.deltaTime;//時間を計測している。
+        AllSeconds+=Time.deltaTime;
         if(Seconds1>=1.5)//１．５秒経ったら。
         {
             float Appearance1 = Random.Range(AppearanceMin,AppearanceMax);//エネミー出現ポイントをランダムで決める。
             Instantiate(Enemy1, new Vector3(RangePointX+1, Appearance1, 0), Quaternion.identity);//エネミー生成。
             Seconds1=0;//時間経過リセッ．ト。
         }
-        if(Seconds2>=1.5)//１．５秒経ったら。
+        if(Seconds2>=1.5&&AllSeconds>=30)//１．５秒経ったら。
         {
             float Appearance2 = Random.Range(AppearanceMin,AppearanceMax);//エネミー出現ポイントをランダムで決める。
             Instantiate(Enemy2, new Vector3(RangePointX+1, Appearance2, 0), Quaternion.identity);//エネミー生成。
             Seconds2=0;//時間経過リセット。
         }
-        if(Seconds3>=1.5)//１．５秒経ったら。
+        if(Seconds3>=1.5&&AllSeconds>=60)//１．５秒経ったら。
         {
             float Appearance3 = Random.Range(AppearanceMin,AppearanceMax);//エネミー出現ポイントをランダムで決める。。
             Instantiate(Enemy3, new Vector3(RangePointX+1, Appearance3, 0), Quaternion.identity);//エネミー生成。
             Seconds3=0;//時間経過リセット。
+            AllSeconds=60;
         }
     }
 }

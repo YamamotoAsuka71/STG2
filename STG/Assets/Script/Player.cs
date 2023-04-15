dce82cd7dd78837a7fe9b3d51e37916d4e0fb97e
itou aiki <itouaiki46@gmail.com>
2023-04-15 11:03:29 +0900

    プレイヤーの移動範囲の設定
    横と縦の移動範囲制限かけました

--- STG/Assets/Script/Player.cs
+++ STG/Assets/Script/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //プレイヤーが移動できるx軸の最大値
+    public float maxwidth=8.388f;
+    //プレイヤーが移動できるx軸の最小値
+    public float minwidth=-8.388f;
+    //プレイヤーが移動できるy軸の最大値
+    public float maxhigh=4.5f;
+    //プレイヤーが移動できるy軸の最小値
+    public float minhigh=-4.5f;
     //動かすプレイヤーのオブジェクト
     public GameObject player;
     //マウスのクリックした座標の取得
@@ -21,27 +29,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move();
+    }
+
+    void move()
+    {
         if(Input.GetMouseButtonDown(0))//左クリックされたら起動
         {
             //マウスでクリックした座標の取得しtouchScreenPositionに格納
             Vector3 touchScreenPosition=Input.mousePosition;
             //プレイヤーの奥行きを5.0に固定
             touchScreenPosition.z=5.0f;
-            Camera camera=Camera.main;//ここ理解できてない
-            //touchWorldPositionに何かを格納？（ここも理解できてない）
+            Camera camera=Camera.main;//カメラの取得
+            //touchWorldPositionにスクリーンポジションをワールドポジションに変換して格納
             touchWorldPosition=camera.ScreenToWorldPoint(touchScreenPosition);
             moveflg=true;
        } 
        if(moveflg==true)
        {
-            //プレイヤーが指定座標に移動（詳しくはわからない）
-            player.transform.position=Vector3.MoveTowards(player.transform.position,touchWorldPosition
-            ,speed*Time.deltaTime*2);
-            if(player.transform.position==touchWorldPosition)
-            {
-                moveflg=false;
-            }
-            
+            //プレイヤーの行動範囲の制限
+            if(touchWorldPosition.x<maxwidth&&touchWorldPosition.x>minwidth&&
+            touchWorldPosition.y<maxhigh&&touchWorldPosition.y>minhigh)
+            //{
+                //プレイヤーが指定座標に移動（詳しくはわからない）
+                player.transform.position=Vector3.MoveTowards(player.transform.position,touchWorldPosition
+                ,speed*Time.deltaTime*2);
+                if(player.transform.position==touchWorldPosition)
+                {
+                    moveflg=false;
+                }            
+            //}            
        }      
+        
     }
 }

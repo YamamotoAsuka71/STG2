23a38d3dc3d31b834628cbbf51ff92c609f21311
YamamotoAsuka <shira71neko@gmail.com>
2023-04-16 00:07:11 +0900

    ミサイルの追尾機能のみの実装。
    方向転換は未実装

---
+++ STG/Assets/Script/MissilSpeed.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissilSpeed : MonoBehaviour
+{
+    private GameObject playerObject;
+    private Vector3 PlayerPosition;
+    private Vector3 EnemyPosition;
+    public float Seconds3=0;
+    bool flg=false;
+    bool flg1=false;
+    bool flg2=false;
+    bool flg3=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindWithTag("Player");
+ 
+        PlayerPosition = playerObject.transform.position;
+        EnemyPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Seconds3+=Time.deltaTime;
+        PlayerPosition = playerObject.transform.position;
+        EnemyPosition = transform.position;
+        if(Seconds3<=5)
+        {
+            //// 対象物へのベクトルを算出
+            //Vector3 toDirection = playerObject.transform.position - transform.position;
+            //// 対象物へ回転する
+            //transform.rotation = Quaternion.FromToRotation(Vector3.up, toDirection);
+
+            if (PlayerPosition.x > EnemyPosition.x&&PlayerPosition.y > EnemyPosition.y)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+            }
+            if (PlayerPosition.x < EnemyPosition.x&&PlayerPosition.y > EnemyPosition.y)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+            }
+
+            if (PlayerPosition.x > EnemyPosition.x&&PlayerPosition.y < EnemyPosition.y)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+            }
+            if (PlayerPosition.x < EnemyPosition.x&&PlayerPosition.y < EnemyPosition.y)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+            }
+ 
+            transform.position = EnemyPosition;
+        }
+        if(Seconds3>5)
+        {
+            if (PlayerPosition.x > EnemyPosition.x&&PlayerPosition.y > EnemyPosition.y&&flg1==false&&flg2==false&&flg3==false)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+                flg=true;
+            }
+            if (PlayerPosition.x < EnemyPosition.x&&PlayerPosition.y > EnemyPosition.y&&flg==false&&flg2==false&&flg3==false)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+                flg1=true;
+            }
+
+            if (PlayerPosition.x > EnemyPosition.x&&PlayerPosition.y < EnemyPosition.y&&flg==false&&flg1==false&&flg3==false)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+                flg2=true;
+            }
+            if (PlayerPosition.x < EnemyPosition.x&&PlayerPosition.y < EnemyPosition.y&&flg==false&&flg1==false&&flg2==false)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+                flg3=true;
+            }
+            if(flg==true)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+            }
+            if(flg1==true)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y + 1f*Time.deltaTime;
+            }
+            if(flg2==true)
+            {
+                EnemyPosition.x = EnemyPosition.x + 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+            }
+            if(flg3==true)
+            {
+                EnemyPosition.x = EnemyPosition.x - 1f*Time.deltaTime;
+                EnemyPosition.y = EnemyPosition.y - 1f*Time.deltaTime;
+            }
+            transform.position = EnemyPosition;
+        }
+        if(Seconds3>=10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ STG/Assets/Script/MissileGenerator.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileGenerator : MonoBehaviour
+{
+
+    float Seconds1=0;
+    float Seconds2=0;
+    float Seconds3=0;
+
+    //float speed=2;
+
+    public GameObject Missile;
+    public GameObject Target;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        Seconds1+=Time.deltaTime;
+        Seconds2+=Time.deltaTime;
+        if(Seconds1>=5)
+        {
+            Seconds3+=Time.deltaTime;
+            if(Seconds2>=5)
+            {
+                Seconds2+=Time.deltaTime;
+                float flgCount=Random.Range(0,2);
+            
+                if(flgCount==0)
+                {
+                    TopMove();
+                }
+                if(flgCount==1)
+                {
+                    UnderMove();
+                }
+            }
+            Seconds2=0;
+        }
+        
+        if(Seconds1>=15)
+        {
+            Seconds1=0;
+            Seconds2=0;
+        }
+        
+    }
+
+    void TopMove()
+    {
+        Instantiate(Missile, new Vector3(0, 5.5f, 0), Quaternion.Euler(0, 0, 0));
+        Missile.SetActive (true);
+    }
+    void UnderMove()
+    {
+        Instantiate(Missile, new Vector3(0, -5.5f, 0), Quaternion.Euler(0, 0, 0));
+        Missile.SetActive (true);
+    }
+    
+}
